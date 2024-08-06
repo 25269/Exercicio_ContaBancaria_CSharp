6949239b15bccd8037434b1ad45ff1cc59d8d850
Cassiano Baldasso <baldassocassiano@gmail.com>
2024-08-06 14:45:48 -0300

    Primeira versão

---
+++ Exercicios/ContaBancaria.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercicios
+{
+    class ContaBancaria
+    {
+        public string NomeTitular { get; set; }
+
+        public int NumeroConta { get; private set; }
+
+        public double Saldo { get; private set; }
+
+        private double TaxaSaque = 5.00;
+
+
+        public ContaBancaria(string nomeTitular, int numeroConta, double saldo) : this(nomeTitular, numeroConta)
+        {
+            Saldo = saldo;
+        }
+
+        public ContaBancaria(string nomeTitular, int numeroConta)
+        {
+            NomeTitular = nomeTitular;
+            NumeroConta = numeroConta;
+        }
+
+        public void SetNomeTitular(string nomeTitular)
+        {
+            NomeTitular = nomeTitular;
+        }
+
+        public string GetNomeTitular()
+        {
+            return NomeTitular;
+        }
+
+        public int GetNumeroConta()
+        {
+            return NumeroConta;
+        }
+
+        public double GetSaldo()
+        {
+            return Saldo;
+        }
+
+        public void AumentaSaldo(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void Saque(double saque)
+        {
+            Saldo -= (saque + TaxaSaque);
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + this.GetNumeroConta() + ", Titular: " + this.GetNomeTitular()
+                + ", Saldo: $ " + this.GetSaldo().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Exercicios/Program.cs
@@ -0,0 +1,55 @@
+﻿using Exercicios;
+using System;
+using System.Globalization;
+
+namespace MeuNamespace
+{
+    //Classe
+    public class Exercicios
+    {
+        static void Main(string[] args)
+        {
+            string NoYes;
+            ContaBancaria contaBancaria;
+
+            Console.Write("Digite o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o titular da conta: ");
+            string nomeTitular = (Console.ReadLine());
+
+            Console.Write("Haverá depósito inicial? (s/n): ");
+            NoYes = Console.ReadLine();
+
+            if (NoYes == "s")
+            {
+                Console.Write("Digite o valor do depósito inicial: ");
+                double deposito = double.Parse(Console.ReadLine());
+                contaBancaria = new ContaBancaria(nomeTitular, numeroConta, deposito);
+            }
+            else {
+                contaBancaria = new ContaBancaria(nomeTitular, numeroConta);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(contaBancaria);
+
+            Console.WriteLine("");
+            Console.Write("Digite um valor de depósito: ");
+            contaBancaria.AumentaSaldo(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(contaBancaria);
+
+            Console.WriteLine("");
+            Console.Write("Digite um valor de saque: ");
+            contaBancaria.Saque(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(contaBancaria);
+        }
+    }
+}
